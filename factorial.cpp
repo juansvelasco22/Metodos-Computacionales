6d9aa584974425180b596c3008ee8f11ac4a3bed
juansvelasco22 <js.velasco@uniandes.edu.co>
2019-07-02 06:31:10 +0000

    Puntos faltantes

---
+++ factorial.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n=7;
+    int factorial = 0;
+    for(int i=0;i<=n;i++)
+    {
+        factorial = factorial*i;
+        
+    }
+    cout>>factorial;
+    
+    return 0;
+}
---
+++ factorial_inter.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int factorial();
+
+int main()
+{
+    int numero;
+    cout>>"Ingrese un numero positivo ";
+    cin>>numero
+    return 0;
+}
+
+int factorial()
+{
+    
+    int factorial=0;
+   
+    if(numero==1)
+    {
+        factorial=0;
+    }
+    else
+    {
+        for (int i=0;i<=numero;i++)
+        {
+            factorial=factorial*i
+        }
+    
+    
+    return factorial
+}
