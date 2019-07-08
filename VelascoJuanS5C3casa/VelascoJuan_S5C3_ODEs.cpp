37d1aad9079ef788fd132b9105402d47924dcbb7
juansvelasco22 <js.velasco@uniandes.edu.co>
2019-07-08 14:48:12 +0000

    ODEs

---
+++ VelascoJuanS5C3casa/VelascoJuan_S5C3_ODEs.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+double Leap-Frog();
+
+int main()
+{
+    double m=2.0;
+    double k=300.0;
+    double h=0.1;
+    double x[];
+    double t[];
+    
+    return 0;
+}
+
+double euler(double m, double k)
+{
+    
+}
+
+double Leap-Frog(double m, double k)
+{
+    double Vx;
+    for(int i=0;i<;i++)
+    {
+        Vx=Vx+x[i+h]+x[i];
+        
+    }
+}
