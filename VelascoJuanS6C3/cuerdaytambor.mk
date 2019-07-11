5667ef056240c897b7d7bcdc01d44d5ffa863d86
juansvelasco22 <js.velasco@uniandes.edu.co>
2019-07-11 01:49:46 +0000

    Cuerda y tambor

---
+++ VelascoJuanS6C3/cuerdaytambor.mk
@@ -0,0 +1,15 @@
+plots_cuerdayTambor.png:caso1.dat caso2.dat caso3.dat plots_cuerdayTambor.py
+    python plots_cuerdayTambor.py
+    
+caso1:a.out
+    ./a.out
+    
+caso2:a.out
+    ./a.out
+
+caso3:a.out
+    ./a.out
+    
+    
+a.out:VelascoJuanS6C3_cuerdayTambor.cpp
+    g++ VelascoJuanS6C3_cuerdayTambor.cpp
