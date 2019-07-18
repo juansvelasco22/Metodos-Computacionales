ee5eacf56547924b04e09f7a1121e3998daa32b8
juansvelasco22 <js.velasco@uniandes.edu.co>
2019-07-18 19:51:06 +0000

    Fourier terminada

--- VelascoJuan_hw2/Fourier.py
+++ VelascoJuan_hw2/Fourier.py
@@ -22,28 +22,27 @@ plt.imshow(np.abs(Fcara2), norm=LogNorm(vmin=1))
 plt.colorbar()
 
 
+
+
 #Tomar de la cara 1 las frecuencias altas
 filas1, columnas1 =Fcara1.shape
 freq1=Fcara1
-freq1[int(filas1*0.4):int(filas1*(1-0.4))] = 0
-freq1[:, int(columnas1*0.4):int(columnas1*(1-0.4))] = 0
+freq1[int(filas1*0.05):int(filas1*(1-0.05))] = 0
+freq1[:, int(columnas1*0.05):int(columnas1*(1-0.05))] = 0
 plt.imshow(abs(freq1), norm=LogNorm(vmin=1))
 plt.colorbar()
 
-#Tomar de la cara 2 las frecuencias bajas
 filas2, columnas2 =Fcara2.shape
 freq2=Fcara2.copy()
-freq2[int(filas2*0.4):int(filas2*(1-0.4))] = 0
-freq2[:,int(columnas2*0.4):int(columnas2*(1-0.4))] = 0
-Filtro = (Fcara2-freq2).real
+freq2[int(filas2*0.05):int(filas2*(1-0.05))] = 0
+freq2[:,int(columnas2*0.05):int(columnas2*(1-0.05))] = 0
+Filtro = (Fcara2-freq2)
 plt.imshow(abs(Filtro), norm=LogNorm(vmin=1))
 plt.colorbar()
 
-#Crear la imagen hibrida
 Nueva=(Filtro+freq1)
 plt.imshow(abs(Nueva), norm=LogNorm(vmin=1))
 plt.colorbar()
 
-#Calcular la transformada inversa
 Inversa=fftpack.ifft2(Nueva)
 plt.imshow(abs(Inversa),plt.cm.gray)
