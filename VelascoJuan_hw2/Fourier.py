a831a7a496125c39e93dd6f92af292e26818f5ea
juansvelasco22 <js.velasco@uniandes.edu.co>
2019-07-19 05:14:55 +0000

    tarea 2

--- VelascoJuan_hw2/Fourier.py
+++ VelascoJuan_hw2/Fourier.py
@@ -1,13 +1,13 @@
 import numpy as np
-import matplotlib.pylab as plt
+import matplotlib.pyplot as plt
 from scipy.fftpack import fft, fftfreq
 from scipy import fftpack
 from matplotlib.colors import LogNorm
 
 
 #Importar los archivos de las fotografias
-cara1=plt.imread("cara_02_grisesMF.png")
-cara2=plt.imread("cara_03_grisesMF.png")
+cara2=plt.imread("cara_02_grisesMF.png")
+cara1=plt.imread("cara_03_grisesMF.png")
 
 
 #Usando los paquetes de Numpy calculamos la transformada de Fourier
@@ -16,11 +16,17 @@ Fcara1=fftpack.fft2(cara1)
 Fcara2=fftpack.fft2(cara2)
 
 #Graficas de los espectros de Fourier de las dos imagenes
-plt.imshow(abs(Fcara1), norm=LogNorm(vmin=1))
-plt.colorbar()
-plt.imshow(np.abs(Fcara2), norm=LogNorm(vmin=1))
-plt.colorbar()
+fig=plt.figure(figsize=(15,5))
+ax1=fig.add_subplot(121)
+ax2=fig.add_subplot(122)
+ax1.imshow(abs(Fcara1), norm=LogNorm(vmin=1))
+ax1.set_title("Espectro de Fourier de cara 1")
 
+ax2.imshow(abs(Fcara2), norm=LogNorm(vmin=1))
+ax2.set_title("Espectro de Fourier de cara 2")
+fig.colorbar(plt.cm.ScalarMappable())
+fig.savefig("Espectros_fourier.pdf")
+plt.close(fig)
 
 
 
@@ -29,20 +35,38 @@ filas1, columnas1 =Fcara1.shape
 freq1=Fcara1
 freq1[int(filas1*0.05):int(filas1*(1-0.05))] = 0
 freq1[:, int(columnas1*0.05):int(columnas1*(1-0.05))] = 0
-plt.imshow(abs(freq1), norm=LogNorm(vmin=1))
-plt.colorbar()
 
+#Tomar las frecuencias bajas de la cara 2
 filas2, columnas2 =Fcara2.shape
 freq2=Fcara2.copy()
 freq2[int(filas2*0.05):int(filas2*(1-0.05))] = 0
 freq2[:,int(columnas2*0.05):int(columnas2*(1-0.05))] = 0
-Filtro = (Fcara2-freq2)
-plt.imshow(abs(Filtro), norm=LogNorm(vmin=1))
-plt.colorbar()
 
+#Graficar los filtros de las caras
+Filtro = (Fcara2-freq2)
+fig=plt.figure(figsize=(15,5))
+ax1=fig.add_subplot(121)
+ax2=fig.add_subplot(122)
+ax1.imshow(abs(freq1), norm=LogNorm(vmin=1))
+ax1.set_title("Filtro de frecuencias altas")
+ax2.imshow(abs(Filtro), norm=LogNorm(vmin=1))
+ax2.set_title("Filtro de frecuencias bajas")
+fig.colorbar(plt.cm.ScalarMappable())
+fig.savefig("Filtros.pdf")
+plt.close(fig)
+
+#Generar el nuevo espectro que corresponde a la suma de los dos filtros
 Nueva=(Filtro+freq1)
 plt.imshow(abs(Nueva), norm=LogNorm(vmin=1))
+plt.title("Espectro con la suma de los filtros")
 plt.colorbar()
+plt.savefig("Espectro_nuevo.pdf")
+plt.close()
 
+
+#Encontrar la transformada inversa para volver a obtener la imagen hibrida
 Inversa=fftpack.ifft2(Nueva)
 plt.imshow(abs(Inversa),plt.cm.gray)
+plt.title("Cara Hibrida")
+plt.savefig("Cara_Hibrida.pdf")
+plt.close()
